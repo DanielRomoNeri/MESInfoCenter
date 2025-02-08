b2a298d745792bc5f4ef2d4fcd4411723f423dfb
Daniel Romo <danielromoneri@gmail.com>
2025-02-07 21:57:49 -0600

    espacios

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,23 +37,23 @@
             this.headerSearchPanel = new System.Windows.Forms.Panel();
             this.tbContentSearch = new System.Windows.Forms.TextBox();
             this.containerPanel = new System.Windows.Forms.Panel();
-            this.lblTitleApp = new System.Windows.Forms.Label();
+            this.menuPanel = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.containerHeaderPanel = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.menuPanel = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.lblTitleApp = new System.Windows.Forms.Label();
             this.sidePanel.SuspendLayout();
             this.panel1.SuspendLayout();
             this.sideBarHeaderPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.headerSearchPanel.SuspendLayout();
             this.containerPanel.SuspendLayout();
+            this.menuPanel.SuspendLayout();
             this.containerHeaderPanel.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
-            this.menuPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // sidePanel
@@ -65,17 +65,17 @@
             this.sidePanel.Location = new System.Drawing.Point(0, 0);
             this.sidePanel.Margin = new System.Windows.Forms.Padding(2);
             this.sidePanel.Name = "sidePanel";
-            this.sidePanel.Size = new System.Drawing.Size(230, 705);
+            this.sidePanel.Size = new System.Drawing.Size(288, 881);
             this.sidePanel.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.flowAppsList);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 125);
+            this.panel1.Location = new System.Drawing.Point(0, 156);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(230, 580);
+            this.panel1.Size = new System.Drawing.Size(288, 725);
             this.panel1.TabIndex = 3;
             // 
             // flowAppsList
@@ -86,7 +86,7 @@
             this.flowAppsList.Location = new System.Drawing.Point(0, 0);
             this.flowAppsList.Margin = new System.Windows.Forms.Padding(2);
             this.flowAppsList.Name = "flowAppsList";
-            this.flowAppsList.Size = new System.Drawing.Size(230, 580);
+            this.flowAppsList.Size = new System.Drawing.Size(288, 725);
             this.flowAppsList.TabIndex = 2;
             this.flowAppsList.WrapContents = false;
             // 
@@ -98,16 +98,16 @@
             this.sideBarHeaderPanel.Location = new System.Drawing.Point(0, 0);
             this.sideBarHeaderPanel.Margin = new System.Windows.Forms.Padding(2);
             this.sideBarHeaderPanel.Name = "sideBarHeaderPanel";
-            this.sideBarHeaderPanel.Size = new System.Drawing.Size(230, 125);
+            this.sideBarHeaderPanel.Size = new System.Drawing.Size(288, 156);
             this.sideBarHeaderPanel.TabIndex = 3;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MESInfoCenter.Properties.Resources.molexLogoTranparente;
-            this.pictureBox1.Location = new System.Drawing.Point(10, 17);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 21);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(204, 36);
+            this.pictureBox1.Size = new System.Drawing.Size(255, 45);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -115,10 +115,10 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(10, 72);
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(205, 30);
+            this.textBox1.Size = new System.Drawing.Size(255, 36);
             this.textBox1.TabIndex = 1;
             // 
             // headerSearchPanel
@@ -126,19 +126,19 @@
             this.headerSearchPanel.BackColor = System.Drawing.Color.Black;
             this.headerSearchPanel.Controls.Add(this.tbContentSearch);
             this.headerSearchPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.headerSearchPanel.Location = new System.Drawing.Point(230, 0);
+            this.headerSearchPanel.Location = new System.Drawing.Point(288, 0);
             this.headerSearchPanel.Margin = new System.Windows.Forms.Padding(2);
             this.headerSearchPanel.Name = "headerSearchPanel";
-            this.headerSearchPanel.Size = new System.Drawing.Size(1198, 63);
+            this.headerSearchPanel.Size = new System.Drawing.Size(1497, 79);
             this.headerSearchPanel.TabIndex = 1;
             // 
             // tbContentSearch
             // 
             this.tbContentSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbContentSearch.Location = new System.Drawing.Point(364, 17);
+            this.tbContentSearch.Location = new System.Drawing.Point(455, 21);
             this.tbContentSearch.Margin = new System.Windows.Forms.Padding(2);
             this.tbContentSearch.Name = "tbContentSearch";
-            this.tbContentSearch.Size = new System.Drawing.Size(455, 30);
+            this.tbContentSearch.Size = new System.Drawing.Size(568, 36);
             this.tbContentSearch.TabIndex = 0;
             // 
             // containerPanel
@@ -148,31 +148,73 @@
             this.containerPanel.Controls.Add(this.containerHeaderPanel);
             this.containerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.containerPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.containerPanel.Location = new System.Drawing.Point(230, 63);
+            this.containerPanel.Location = new System.Drawing.Point(288, 79);
+            this.containerPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.containerPanel.Name = "containerPanel";
-            this.containerPanel.Size = new System.Drawing.Size(1198, 642);
+            this.containerPanel.Size = new System.Drawing.Size(1497, 802);
             this.containerPanel.TabIndex = 2;
             // 
-            // lblTitleApp
+            // menuPanel
             // 
-            this.lblTitleApp.AutoSize = true;
-            this.lblTitleApp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblTitleApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitleApp.ForeColor = System.Drawing.Color.White;
-            this.lblTitleApp.Location = new System.Drawing.Point(3, 0);
-            this.lblTitleApp.Name = "lblTitleApp";
-            this.lblTitleApp.Size = new System.Drawing.Size(1192, 81);
-            this.lblTitleApp.TabIndex = 0;
-            this.lblTitleApp.Text = "[TITULO DE LA APLICACIÓN]";
-            this.lblTitleApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.menuPanel.Controls.Add(this.button3);
+            this.menuPanel.Controls.Add(this.button4);
+            this.menuPanel.Controls.Add(this.button2);
+            this.menuPanel.Controls.Add(this.button1);
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuPanel.Location = new System.Drawing.Point(0, 101);
+            this.menuPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(195, 701);
+            this.menuPanel.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 185);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Copiar ruta al portapapeles";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 134);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 29);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Solución de problemas";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 80);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "¿Cómo funciona?";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Informacion General";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // containerHeaderPanel
             // 
             this.containerHeaderPanel.Controls.Add(this.tableLayoutPanel1);
             this.containerHeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.containerHeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.containerHeaderPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.containerHeaderPanel.Name = "containerHeaderPanel";
-            this.containerHeaderPanel.Size = new System.Drawing.Size(1198, 81);
+            this.containerHeaderPanel.Size = new System.Drawing.Size(1497, 101);
             this.containerHeaderPanel.TabIndex = 1;
             // 
             // tableLayoutPanel1
@@ -182,67 +224,34 @@
             this.tableLayoutPanel1.Controls.Add(this.lblTitleApp, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1198, 81);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1497, 101);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // menuPanel
+            // lblTitleApp
             // 
-            this.menuPanel.Controls.Add(this.button3);
-            this.menuPanel.Controls.Add(this.button4);
-            this.menuPanel.Controls.Add(this.button2);
-            this.menuPanel.Controls.Add(this.button1);
-            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.menuPanel.Location = new System.Drawing.Point(0, 81);
-            this.menuPanel.Name = "menuPanel";
-            this.menuPanel.Size = new System.Drawing.Size(156, 561);
-            this.menuPanel.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(5, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Informacion General";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(5, 64);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "¿Cómo funciona?";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(5, 107);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 23);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "Solución de problemas";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(5, 148);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 42);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Copiar ruta al portapapeles";
-            this.button3.UseVisualStyleBackColor = true;
+            this.lblTitleApp.AutoSize = true;
+            this.lblTitleApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitleApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleApp.ForeColor = System.Drawing.Color.White;
+            this.lblTitleApp.Location = new System.Drawing.Point(4, 0);
+            this.lblTitleApp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitleApp.Name = "lblTitleApp";
+            this.lblTitleApp.Size = new System.Drawing.Size(1489, 101);
+            this.lblTitleApp.TabIndex = 0;
+            this.lblTitleApp.Text = "[TITULO DE LA APLICACIÓN]";
+            this.lblTitleApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.LightSlateGray;
-            this.ClientSize = new System.Drawing.Size(1428, 705);
+            this.ClientSize = new System.Drawing.Size(1785, 881);
             this.Controls.Add(this.containerPanel);
             this.Controls.Add(this.headerSearchPanel);
             this.Controls.Add(this.sidePanel);
@@ -258,10 +267,10 @@
             this.headerSearchPanel.ResumeLayout(false);
             this.headerSearchPanel.PerformLayout();
             this.containerPanel.ResumeLayout(false);
+            this.menuPanel.ResumeLayout(false);
             this.containerHeaderPanel.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            this.menuPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
