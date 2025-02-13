1e4bf55a1319e61652d4ec8fb31ca1b0b37fbf6e
Daniel Romo <danielromoneri@gmail.com>
2025-02-13 16:24:16 -0600

    Se agrega clase para conexión a base de datos

---
+++ DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESInfoCenter
+{
+    internal class DBConnection
+    {
+        private static readonly string connectionString = "server=localhost;database=testdb;user=root;password=tucontraseña;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
