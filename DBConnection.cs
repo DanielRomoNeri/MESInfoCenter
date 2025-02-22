21609a56b0f8c9c7e44099dde3512353f29c5cd4
Daniel Romo <danielromoneri@gmail.com>
2025-02-21 21:08:01 -0600

    .

---
+++ DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using MySqlConnector;
+namespace MESInfoCenter
+{
+    internal class DBConnection
+    {
+        private static readonly string connectionString = "server=server;database=database;user=user;password=password;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
